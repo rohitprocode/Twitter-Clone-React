0b25e7a3343246626a0d7e0cb657b68a1b552295
rohitprocode <rohit990797@gmail.com>
2023-12-06 11:52:50 +0530

    MidContainerHeader

---
+++ src/components/MidContainer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './MidContainer.css'
+
+function MidContainer() {
+  return (
+    <div className='MidContainer'>
+      <div className="header">
+        <h3 id='foryou'>For you</h3>
+        <h3 id='following' >Following</h3>
+        <img src="../Setting.png" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default MidContainer
