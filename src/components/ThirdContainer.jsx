99e07dfd30d6d38b0d6ec0b2e177e960c31e237e
rohitprocode <rohit990797@gmail.com>
2023-12-12 01:25:10 +0530

    ThirdContainer

---
+++ src/components/ThirdContainer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './ThirdContainer.css'
+
+function ThirdContainer() {
+  return (
+    <div className='ThirdContainer'>
+      <div className="topSearchBar">
+        <img src="Explore2.png" alt="" />
+        <input type="search" placeholder='Search' />
+      </div>
+    </div>
+  )
+}
+
+export default ThirdContainer
