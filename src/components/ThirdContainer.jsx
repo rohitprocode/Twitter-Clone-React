faa2d69c5270494a9967d9b2eb354d1572769903
rohitprocode <rohit990797@gmail.com>
2023-12-14 00:07:35 +0530

    Trending

--- src/components/ThirdContainer.jsx
+++ src/components/ThirdContainer.jsx
@@ -16,8 +16,22 @@ function ThirdContainer() {
           <p>Subscribe to unlock new features and if eligible, receive a share of ads revenue.</p>
           <button id="SubscribeBtn" >Subscribe</button>
         </div>
-        {/* <div className="TrendingBox">b</div>
-        <div className="FollowBox">c</div> */}
+        <div className="TrendingBox">
+          <h1>What's happening</h1>
+          <div className="Trend-Container">
+            <div>
+            <span>Esports · December 11, 2023</span>
+            <h4>TFT Vegas Open Finals</h4>
+            </div>  
+            <img src="../IPL.jpg" alt="" />
+          </div>
+          <div className="Trend-Container">a</div>
+          <div className="Trend-Container">b</div>
+          {/* <div className="Trend-Container">c</div> */}
+          {/* <div className="Trend-Container">d</div> */}
+          {/* <div className="Trend-Container">e</div> */}
+        </div>
+        {/* <div className="FollowBox">c</div> */}
       </div>
     </div>
   );
