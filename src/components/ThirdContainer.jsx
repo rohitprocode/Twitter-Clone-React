b6b7de049fdcfd7d941fc5fec3460e80799940ae
rohitprocode <rohit990797@gmail.com>
2023-12-13 15:51:26 +0530

    ThirdContainer

--- src/components/ThirdContainer.jsx
+++ src/components/ThirdContainer.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
-import './ThirdContainer.css'
+import React from "react";
+import "./ThirdContainer.css";
 
 function ThirdContainer() {
   return (
-    <div className='ThirdContainer'>
+    <div className="ThirdContainer">
       <div className="topSearchBar">
         <div className="imgContent">
-        <img src="Explore2.png" alt="" />
+          <img src="Explore2.png" alt="" />
         </div>
-        <input type="search" placeholder='Search' />
+        <input type="search" placeholder="Search" />
+      </div>
+      <div className="bottomContainer">
+        <div className="PremiumBox">
+          <h1>Subscribe to Premium</h1>
+          <p>Subscribe to unlock new features and if eligible, receive a share of ads revenue.</p>
+          <button id="SubscribeBtn" >Subscribe</button>
+        </div>
+        {/* <div className="TrendingBox">b</div>
+        <div className="FollowBox">c</div> */}
       </div>
     </div>
-  )
+  );
 }
 
-export default ThirdContainer
+export default ThirdContainer;
