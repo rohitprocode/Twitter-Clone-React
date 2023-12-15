199e8d420f01b03de540daed135197a39f9d4d8e
rohitprocode <rohit990797@gmail.com>
2023-12-15 20:25:50 +0530

    ContentContainer

---
+++ src/components/ContentBox.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ContentContainer.css'
+
+function ContentBox() {
+  return (
+    <div className='ContentContainer'>
+    <h1>ContentBox</h1>  
+    </div>
+  )
+}
+
+export default ContentBox
